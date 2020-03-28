1acba72aec962be295c67e753531881f4b4a9db9
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-28 23:23:18 +0900

    Added navigations and modified files in order to use React navigation

---
+++ src/navigations/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '@sreens/home';
+
+const Stack = createStackNavigator();
+
+const Navigator: React.FC<{}> = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default Navigator;
