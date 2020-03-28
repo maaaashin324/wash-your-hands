d851568cd539d495882e1d4183150ade75be45a6
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-28 23:55:41 +0900

    Fixed a typo

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
           alias: {
             '@components': './src/components',
             '@navigations': './src/navigations',
-            '@sreens': './src/screens',
+            '@screens': './src/screens',
           },
         },
       ],
