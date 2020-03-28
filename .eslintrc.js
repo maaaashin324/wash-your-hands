100aca71ad57b8b94a1527b9f6638af8bcdf0ded
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-28 20:56:11 +0900

    Modified eslintrc in order to activate eslint-config-airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,6 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint"
-  ]
+  // https://medium.com/@killerchip0/react-native-typescript-with-eslint-and-prettier-e98d50585627
+  extends: ["airbnb-typescript", "prettier", "prettier/@typescript-eslint"]
 };
