3b8e30d4790a2271ac0849758ba2e4efd1fde5b0
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-28 23:14:02 +0900

    Added HomeScreen

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const HomeScreen: React.FC<{}> = () => (
+  <View style={styles.container}>
+    <Text style={styles.header}>Wash your hands</Text>
+    <Text style={styles.sentence}>
+      Why not wash your hands to guard you from COVID19?
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  header: {
+    fontSize: 100,
+  },
+  sentence: {
+    fontSize: 50,
+  },
+});
