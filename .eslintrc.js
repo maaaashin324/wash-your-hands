1236073aec6179d2ff7469dd25f7af0a3d5f9bf3
Masataka Shintoku <masataka.shintoku@gmail.com>
2020-03-28 21:13:05 +0900

    Added eslint and prettier (#2)
    * Added eslint
    
    * Added .eslintrc
    
    * Added .eslintignore
    
    * Added eslint-config-prettier
    
    * Modified eslintrc in order to activate eslint settings for prettier
    
    * Added eslint-config-airbnb-typescript
    
    * Modified eslintrc in order to activate eslint-config-airbnb-typescript
    
    * Added prettier
    
    * Added prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  // https://medium.com/@killerchip0/react-native-typescript-with-eslint-and-prettier-e98d50585627
+  extends: ["airbnb-typescript", "prettier", "prettier/@typescript-eslint"]
+};
