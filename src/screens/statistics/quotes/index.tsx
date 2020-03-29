7d427ed8e8d4324ffce4fc4289afc4a50bf80bb3
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:54:22 +0900

    Added inteface and props to Quotes component

--- src/screens/statistics/quotes/index.tsx
+++ src/screens/statistics/quotes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { View, ViewStyle } from 'react-native';
 import { Subheading, Paragraph } from 'react-native-paper';
 
-const Quotes: React.FC<{}> = () => (
-  <>
+const Quotes: React.FC<{ style?: ViewStyle }> = ({ style }) => (
+  <View style={style}>
     <Subheading>Quotes</Subheading>
     <Paragraph>CDC: Show Me the Science - Why Wash Your Hands?</Paragraph>
     <Paragraph>https://www.cdc.gov/handwashing/why-handwashing.html</Paragraph>
-  </>
+  </View>
 );
 
 export default Quotes;
