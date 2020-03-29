7576dec4743ce1aca3e46ae9dff58d3063504d7c
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:08:20 +0900

    Added Quotes component

---
+++ src/screens/statistics/quotes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Subheading, Paragraph } from 'react-native-paper';
+
+const Quotes: React.FC<{}> = () => (
+  <>
+    <Subheading>Quotes</Subheading>
+    <Paragraph>CDC: Show Me the Science - Why Wash Your Hands?</Paragraph>
+    <Paragraph>https://www.cdc.gov/handwashing/why-handwashing.html</Paragraph>
+  </>
+);
+
+export default Quotes;
