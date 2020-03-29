9166325b52c998f3e7936a06c1f1607cf6e737fb
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:05:34 +0900

    Added SubTitle to DataAboutPrevention

--- src/screens/statistics/prevention/index.tsx
+++ src/screens/statistics/prevention/index.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import { DataTable } from 'react-native-paper';
+import { DataTable, Subheading } from 'react-native-paper';
 
 const DataAboutPrevention: React.FC<{}> = () => (
-  <DataTable>
-    <DataTable.Header>
-      <DataTable.Title>Reduces...</DataTable.Title>
-      <DataTable.Title numeric>Data</DataTable.Title>
-    </DataTable.Header>
+  <>
+    <Subheading>Data about Prevention</Subheading>
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Reduces...</DataTable.Title>
+        <DataTable.Title numeric>Data</DataTable.Title>
+      </DataTable.Header>
 
-    <DataTable.Row>
-      <DataTable.Cell>Sick with diarrhea</DataTable.Cell>
-      <DataTable.Cell numeric>23-40%</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Sick with diarrhea</DataTable.Cell>
+        <DataTable.Cell numeric>23-40%</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Sick with diarrhea/weakened immune</DataTable.Cell>
-      <DataTable.Cell numeric>58%</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Sick with diarrhea/weakened immune</DataTable.Cell>
+        <DataTable.Cell numeric>58%</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Respiratory illnesses</DataTable.Cell>
-      <DataTable.Cell numeric>16-21%</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Respiratory illnesses</DataTable.Cell>
+        <DataTable.Cell numeric>16-21%</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Absenteeism in schoolchildren</DataTable.Cell>
-      <DataTable.Cell numeric>29-57%</DataTable.Cell>
-    </DataTable.Row>
-  </DataTable>
+      <DataTable.Row>
+        <DataTable.Cell>Absenteeism in schoolchildren</DataTable.Cell>
+        <DataTable.Cell numeric>29-57%</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+  </>
 );
 
 export default DataAboutPrevention;
