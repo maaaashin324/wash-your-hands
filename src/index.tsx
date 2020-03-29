4481c4373da0d3ede0721fcba56898b7e6b36701
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 09:54:54 +0900

    Fixed according to lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Navigator from '@navigations/index';
 
-export default function App() {
-  return <Navigator />;
-}
+const MyApp: React.FC<{}> = () => <Navigator />;
+
+export default MyApp;
