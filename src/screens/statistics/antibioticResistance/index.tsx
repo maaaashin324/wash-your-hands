6fd235fa2e609b36d75437343a613d28d0e2f6c5
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:02:54 +0900

    Added antibioticResistance component

---
+++ src/screens/statistics/antibioticResistance/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title, Paragraph } from 'react-native-paper';
+
+const AntibioticResistance: React.FC<{}> = () => (
+  <>
+    <Title>Antibiotic Resistance</Title>
+    <Paragraph>
+      You can help prevent the overuse of antibiotics—the single most important
+      factor leading to antibiotic resistance around the world.
+    </Paragraph>
+  </>
+);
+
+export default AntibioticResistance;
