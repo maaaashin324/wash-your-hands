8a28b2d410cbcde1bedb2d44310403d56cb3aa7e
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 09:50:55 +0900

    Enabled eslint to resolve alias path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,29 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
   // https://medium.com/@killerchip0/react-native-typescript-with-eslint-and-prettier-e98d50585627
-  extends: ["airbnb-typescript", "prettier", "prettier/@typescript-eslint"]
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '@navigations': './src/navigations',
+          '@screens': './src/screens',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
