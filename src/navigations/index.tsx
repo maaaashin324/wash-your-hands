1a643299a7da02ebb6f1914003215611a5bad3d3
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 11:20:12 +0900

    Added StatisticsScreen

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@screens/home';
 import HowToWashScreen from '@screens/howToWash';
+import StatisticsScreen from '@screens/statistics';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ const Navigator: React.FC<{}> = () => (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="HowToWash" component={HowToWashScreen} />
+      <Tab.Screen name="Statistics" component={StatisticsScreen} />
     </Tab.Navigator>
   </NavigationContainer>
 );
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,3 @@
 export { default as HomeScreen } from './home';
 export { default as HowToWashScreen } from './howToWash';
+export { default as StatisticsScreen } from './statistics';
