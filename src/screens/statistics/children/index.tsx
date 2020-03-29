7e9b6e6570ecca48de866c1cc79db8bc1b952891
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:04:18 +0900

    Added Subtitle to DataAboutChildren

--- src/screens/statistics/children/index.tsx
+++ src/screens/statistics/children/index.tsx
@@ -1,42 +1,45 @@
 import React from 'react';
-import { DataTable } from 'react-native-paper';
+import { DataTable, Subheading } from 'react-native-paper';
 
 const DataAboutChildren: React.FC<{}> = () => (
-  <DataTable>
-    <DataTable.Header>
-      <DataTable.Title>Label</DataTable.Title>
-      <DataTable.Title numeric>Data</DataTable.Title>
-    </DataTable.Header>
+  <>
+    <Subheading>Data about children</Subheading>
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Label</DataTable.Title>
+        <DataTable.Title numeric>Data</DataTable.Title>
+      </DataTable.Header>
 
-    <DataTable.Row>
-      <DataTable.Cell>
-        Reduce young children who get sick with diarrhea
-      </DataTable.Cell>
-      <DataTable.Cell>33%</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>
+          Reduce young children who get sick with diarrhea
+        </DataTable.Cell>
+        <DataTable.Cell>33%</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Reduce young children with respiratory</DataTable.Cell>
-      <DataTable.Cell>25%</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Reduce young children with respiratory</DataTable.Cell>
+        <DataTable.Cell>25%</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Improve attendance in school</DataTable.Cell>
-      <DataTable.Cell>Some extend</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Improve attendance in school</DataTable.Cell>
+        <DataTable.Cell>Some extend</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>
-        Improve child development in some settings
-      </DataTable.Cell>
-      <DataTable.Cell>Some extend</DataTable.Cell>
-    </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>
+          Improve child development in some settings
+        </DataTable.Cell>
+        <DataTable.Cell>Some extend</DataTable.Cell>
+      </DataTable.Row>
 
-    <DataTable.Row>
-      <DataTable.Cell>Handwashing after using the toilet</DataTable.Cell>
-      <DataTable.Cell>Only 19%!!</DataTable.Cell>
-    </DataTable.Row>
-  </DataTable>
+      <DataTable.Row>
+        <DataTable.Cell>Handwashing after using the toilet</DataTable.Cell>
+        <DataTable.Cell>Only 19%!!</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+  </>
 );
 
 export default DataAboutChildren;
