ac1b3a482be418d41190d2f4f32115002cdf5631
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 13:53:16 +0900

    Added inteface and props to AntibioticsResistance props

--- src/screens/statistics/antibioticResistance/index.tsx
+++ src/screens/statistics/antibioticResistance/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { View, ViewStyle } from 'react-native';
 import { Subheading, Paragraph } from 'react-native-paper';
 
-const AntibioticResistance: React.FC<{}> = () => (
-  <>
+const AntibioticResistance: React.FC<{ style?: ViewStyle }> = ({ style }) => (
+  <View style={style}>
     <Subheading>Antibiotic Resistance</Subheading>
     <Paragraph>
       You can help prevent the overuse of antibiotics—the single most important
       factor leading to antibiotic resistance around the world.
     </Paragraph>
-  </>
+  </View>
 );
 
 export default AntibioticResistance;
