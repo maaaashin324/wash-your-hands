25cdae89591bb9326980816fcca4429be8aad8f1
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-29 09:53:39 +0900

    Added DetailsScreen to Navigator component

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '@screens/home';
+import DetailsScreen from '@screens/details';
 
 const Stack = createStackNavigator();
 
 const Navigator: React.FC<{}> = () => (
   <NavigationContainer>
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
