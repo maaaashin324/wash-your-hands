874b71260b829c98d88670d01928aa08dc0bd480
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-31 10:41:28 +0900

    Enabled to render Dialog when permission is not granted

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { askPermitLocation } from '@utils/askPermitLocation';
+import { Button, Paragraph, Dialog, Portal } from 'react-native-paper';
+import { enableLocationPermission } from '@utils/askPermitLocation';
 import { askPermitNotifications } from '@utils/askPermitNotifications';
 import startLocationUpdates from '@utils/startLocationUpdates';
 
@@ -20,9 +21,27 @@ const styles = StyleSheet.create({
 });
 
 const HomeScreen: React.FC<{}> = () => {
+  const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
+
+  const hideDialog = (): void => {
+    setDialogOpen(false);
+  };
+
+  const changePermissionFromDialog = async (): Promise<void> => {
+    await enableLocationPermission(true);
+    setDialogOpen(false);
+  };
+
+  const judgeLocationPermissionWhenRendered = async (): Promise<void> => {
+    const result = await enableLocationPermission(true);
+    if (!result) {
+      setDialogOpen(result);
+    }
+  };
+
   useEffect(() => {
     askPermitNotifications(true);
-    askPermitLocation(true);
+    judgeLocationPermissionWhenRendered();
     startLocationUpdates();
   }, []);
 
@@ -32,6 +51,21 @@ const HomeScreen: React.FC<{}> = () => {
       <Text style={styles.sentence}>
         Why not wash your hands to guard you from COVID19?
       </Text>
+      <Portal>
+        <Dialog visible={isDialogOpen} onDismiss={hideDialog}>
+          <Dialog.Title>Permission not granted</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>
+              Please let this app use notification and location to have you wash
+              your hands
+            </Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={changePermissionFromDialog}>Ok</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
