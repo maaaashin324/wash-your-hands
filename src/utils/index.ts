a126de43a73e5cafb7d51866857c9c393d84d829
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-31 10:54:10 +0900

    Modified index in utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
-export { getLocationPermission, askPermitLocation } from './PermissionLocation';
-export {
-  getNotificationPermission,
-  askPermitNotifications,
-} from './PermissionNotification';
 export { default as hasStartedLocationUpdates } from './hasStartedLocationUpdates';
+export * from './PermissionLocation';
+export * from './PermissionNotification';
+export * from './permissions';
 export { default as startLocationUpdates } from './startLocationUpdates';
