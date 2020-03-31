f44ca1dc070ffe5657b73e320aef1fa2ce708eab
maaaashin324 <masataka.shintoku@gmail.com>
2020-03-31 15:39:03 +0900

    Enabled to get high quality thumbnail

--- src/constants/videos.ts
+++ src/constants/videos.ts
@@ -3,18 +3,18 @@ export default [
     title: 'Wash Your Hands',
     subTitle: 'ジャニーズ公式',
     videoURL: 'https://youtu.be/j3KL3BFFbdA?t=816',
-    thumbnail: 'https://img.youtube.com/vi/j3KL3BFFbdA/default.jpg',
+    thumbnail: 'https://img.youtube.com/vi/j3KL3BFFbdA/hqdefault.jpg',
   },
   {
     title: 'Coronavirus | How to wash your hands',
     subTitle: 'Babylon Health',
     videoURL: 'https://youtu.be/seA1wbXUQTs',
-    thumbnail: 'https://img.youtube.com/vi/seA1wbXUQTs/default.jpg',
+    thumbnail: 'https://img.youtube.com/vi/seA1wbXUQTs/hqdefault.jpg',
   },
   {
     title: 'Hand-washing Steps Using the WHO Technique',
     subTitle: 'Johns Hopkins Medicine',
     videoURL: 'https://youtu.be/IisgnbMfKvI',
-    thumbnail: 'https://img.youtube.com/vi/IisgnbMfKvI/default.jpg',
+    thumbnail: 'https://img.youtube.com/vi/IisgnbMfKvI/hqdefault.jpg',
   },
 ];
