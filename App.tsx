5f8282ae504dd8d3ba0f26a3ca5a42aca414c3ac
maaaashin324 <masataka.shintoku@gmail.com>
2020-04-04 16:22:59 +0900

    Added splash and appLoading

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { View, Image } from 'react-native';
+import { AppLoading, SplashScreen } from 'expo';
+import { Asset } from 'expo-asset';
 import * as TaskManager from 'expo-task-manager';
 import * as BackgroundFetch from 'expo-background-fetch';
 import { Provider as PaperProvider } from 'react-native-paper';
@@ -7,7 +10,50 @@ import { makeNotifications, makeTimerNotifications } from '@utils/task';
 import MyApp from './src';
 
 const App: React.FC<{}> = () => {
-  useEffect(() => {});
+  const [isSplashReady, setSplashReady] = useState<boolean>(false);
+  const [isAppReady, setAppReady] = useState<boolean>(false);
+
+  const cacheSplashResourcesAsync = async (): Promise<void> => {
+    // eslint-disable-next-line
+    const png = require('./src/assets/splash.png');
+    return Asset.fromModule(png).downloadAsync();
+  };
+
+  const cacheResourcesAsync = async (): Promise<void> => {
+    SplashScreen.hide();
+    const images = [
+      // eslint-disable-next-line
+      require('./src/assets/icon.png'),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    await Promise.all(cacheImages);
+    setAppReady(true);
+  };
+
+  if (!isSplashReady) {
+    return (
+      <AppLoading
+        startAsync={cacheSplashResourcesAsync}
+        onFinish={(): void => setSplashReady(true)}
+        autoHideSplash={false}
+      />
+    );
+  }
+  if (!isAppReady) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image
+          // eslint-disable-next-line
+          source={require('./src/assets/splash.png')}
+          onLoad={cacheResourcesAsync}
+        />
+      </View>
+    );
+  }
   return (
     <PaperProvider>
       <MyApp />
