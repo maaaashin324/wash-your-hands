b5f317a9c6b20d27ec00d08fdc8f3e9b18d71bb4
maaaashin324 <masataka.shintoku@gmail.com>
2020-04-19 20:06:28 +0900

    Added a test for YouTubeWebView component

---
+++ __test__/components/youTubeWebView/youTubeWebView.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import YouTubeWebView from '@/components/youTubeWebView';
+import videos from '@/constants/videos';
+
+describe('YouTubeWebView', () => {
+  test('should render YouTubeWebView correctly', () => {
+    const tree = renderer
+      .create(<YouTubeWebView uri={videos[0].videoURL} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/utils/location.ts
+++ src/utils/location.ts
@@ -1,10 +1,10 @@
 import * as Location from 'expo-location';
-import { GET_LOCATION_TASK } from '@constants/task';
+import { LOCATION_TASK_NAME } from '@constants/task';
 import { getLocationPermission } from './permissions';
 
 export const hasStartedLocationUpdates = async (): Promise<boolean> => {
   const result = await Location.hasStartedLocationUpdatesAsync(
-    GET_LOCATION_TASK
+    LOCATION_TASK_NAME
   );
   return result;
 };
