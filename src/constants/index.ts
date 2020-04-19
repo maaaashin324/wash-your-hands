58a25db5868c9e8ff98bc4554f745e5ab5f91289
maaaashin324 <masataka.shintoku@gmail.com>
2020-04-19 12:44:29 +0900

    Added layout to constants and use that

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+export { default as Colors } from './colors';
+export { default as Layout } from './layout';
 export * from './navigations';
 export * from './task';
 export { default as VIDEOS } from './videos';
